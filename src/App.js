fe0c9918e6b7839b5a94917582dc681a7ad0d5c6
AJ Auntor <auntorknocks@gmail.com>
2021-03-16 15:41:23 +0600

    added google provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import firebase from "firebase/app";
 import './App.css';
+import "firebase/auth";
+import firebaseConfig from "./firebase.config";
+
+
+firebase.initializeApp(firebaseConfig);
+
+
 
 function App() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  const handleGoogleSingIn = () => {
+
+  }
+
+
   return (
     <div className="App">
       
+    <button onClick={handleGoogleSingIn}>Sign in google</button>
+
     </div>
   );
 }
