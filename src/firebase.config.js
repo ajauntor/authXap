fe180cd986734e7c23c1c95de6e386771dabdd2b
AJ Auntor <auntorknocks@gmail.com>
2021-03-16 15:08:09 +0600

    firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBeEJWIit3PYai9S7fN9TLrfIja8_Jhmx8",
+    authDomain: "authxap.firebaseapp.com",
+    projectId: "authxap",
+    storageBucket: "authxap.appspot.com",
+    messagingSenderId: "299354361741",
+    appId: "1:299354361741:web:57a2f2fa40da248888f7c4"
+  };
+  export default firebaseConfig;
