12005a4689327695dc74dc07c21646b15b8a1c1c
AJ Auntor <auntorknocks@gmail.com>
2021-03-16 21:22:58 +0600

    almost done

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ firebase.initializeApp(firebaseConfig);
 function App() {
   const [user, setUser] = useState({});
   var provider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
   const handleGoogleSingIn = () => {
     firebase.auth()
   .signInWithPopup(provider)
@@ -35,17 +36,40 @@ function App() {
     console.log(errorCode, errorMessage, email, credential )
   });
   }
+const handleFacebookSignIn = () => {
+  firebase
+  .auth()
+  .signInWithPopup(fbProvider)
+  .then((result) => {
+    var credential = result.credential;
+    var user = result.user;
+    var accessToken = credential.accessToken;
+    console.log(user);
+    setUser(user);
+  })
 
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    console.log(errorCode, errorMessage, email, credential);
+  });
+}
 
   return (
     <div className="App">
-
-      <h2>User log in or sign up</h2>
+      <h2 style={{fontSize:"50px", letterSpacing:"6px", fontVariant:"small-caps", color:"whitesmoke"}}>User log in or sign up</h2>
       
-    <button onClick={handleGoogleSingIn}>Sign in google</button>
+    <button style={{padding:"8px 16px"}} onClick={handleGoogleSingIn}>Sign in google</button>
+    <br/>
+    <button style={{padding:"8px 16px", marginTop:"5px"}} onClick={handleFacebookSignIn}>login using facebook</button>
 
-    <h1>Welcome, {user.displayName}</h1>
+    <h1 style={{color:"whitesmoke"}} > Welcome, <span style={{color:"blue"}}> {user.displayName} </span> </h1>
     <img src={user.photoURL} alt=""/>
+
+
+    
 
     </div>
   );
